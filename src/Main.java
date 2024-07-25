cf30ce260a1abbb61d988c8e731c45a81af3264b
amurph37 <amurph.910@gmail.com>
2024-07-25 14:49:36 -0400

    C-O-C Practice Final

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         // Create two BankAccount objects
-        BankAccount account1 = new BankAccount("Alice", 5000);
-        BankAccount account2 = new BankAccount("Bob", 300);
+        BankAccount account1 = new BankAccount("Alice", 500);
+        BankAccount account2 = new BankAccount("Bob", 5000);
+        BankAccount account3 = new BankAccount("Charlie", 300);
 
         // Withdraw $100 from the first account
-        account1.withdraw(100);
+        account2.withdraw(100);
 
         // Deposit $100 into the second account
-        account2.deposit(100);
+        account1.deposit(100);
+        account3.deposit(100);
 
         // Print account details for both accounts
         account1.printAccountDetails();
         account2.printAccountDetails();
+        account3.printAccountDetails();
     }
 }
